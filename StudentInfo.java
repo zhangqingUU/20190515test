e21bf333e9fe9c8bc23a0362275fb95a16f27fd0
zhangqingUU <50359052+zhangqingUU@users.noreply.github.com>
2019-05-15 16:35:37 +0800

    111
    222

---
+++ StudentInfo.java
@@ -0,0 +1,14 @@
+package cn.kgc.tangcco.zhangqing.entity;
+
+import lombok.Data;
+
+@Data
+public class StudentInfo {
+	private int no;
+	private String name;
+	
+	public void showTest() {
+		System.out.println("你好！");
+	}
+	
+}
